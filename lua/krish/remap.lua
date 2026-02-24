d8dd9f988fb80087a821dc21b06e5616f85baccf
Gokulakrishnan <krishgokul351@gmail.com>
2026-02-25 00:09:39 +0530

    add shct for term and sp nav

--- lua/krish/remap.lua
+++ lua/krish/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("n", "<leader>t", ':10sp | term<CR>')
+vim.keymap.set("t", "jj", [[<C-\><C-n>]])
+vim.keymap.set("n", [[<C-j>]], [[<C-W>j]])
+vim.keymap.set("n", [[<C-k>]], [[<C-W>k]])
+vim.keymap.set("n", [[<C-n>]], [[<C-W>w]])
--- lua/krish/set.lua
+++ lua/krish/set.lua
@@ -2,3 +2,5 @@ vim.opt.nu = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.opt.splitbelow = true
